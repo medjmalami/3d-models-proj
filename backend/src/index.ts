df4c071df989f906ac770bf05c842cf72c42a8da
amine <jmalmohamedamine1@gmail.com>
2025-03-27 16:21:14 +0100

    deploying back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,8 +30,7 @@ app.use("/", SignRoutes);
 app.use("/", ModelRoutes);
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
 
 
 
