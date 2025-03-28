c322c559817c17b75f4f28ddf35602f4af22880b
amine <jmalmohamedamine1@gmail.com>
2025-03-28 02:27:27 +0100

    fix vercel deployment

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import { config } from "dotenv";
+config();
 import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './drizzle',
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 // CORS middleware configuration
 app.use(cors({
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: `${process.env.FRONTEND_URL}` || '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
