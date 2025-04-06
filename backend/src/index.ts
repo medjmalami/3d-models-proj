e0c1beb2ac402b3747539f0c0d02b4ba55a34dbb
amine <jmalmohamedamine1@gmail.com>
2025-04-06 20:31:41 +0100

    back to deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import { config } from "dotenv";
 import SignRoutes from "./routes/sign.routes";
 import ModelRoutes from "./routes/models.routes";
-import path from 'path';
 const morgan = require('morgan');
 
 config();
@@ -13,12 +12,10 @@ app.use(express.json());
 // CORS middleware configuration
 app.use(cors({
   origin: `${process.env.FRONTEND_URL}` || '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Static file serving setup
-app.use('/uploads', express.static(path.join(__dirname, '..', 'public', 'uploads', 'models')));
 
 app.use(morgan('tiny'));
 
@@ -28,12 +25,16 @@ app.use("/", ModelRoutes);
 app.get("*", (req , res) => {
   res.status(404).json({ error: "Route not found" });
 })
-
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 app.listen( 5000, () => {
-  console.log(`Server is running on port 5000`);
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
-export default app;
+
+// Also support ES modules
+export default app
 
 
 
