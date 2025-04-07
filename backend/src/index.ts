338a2ad951b6f4256cd7a70ac31cad376d416c31
med amine jmal <jmalmohamedamine1@gmail.com>
2025-04-07 13:45:37 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 // CORS middleware configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL ,
   methods: ['GET', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
